444d7f52495fbc42a8b1614163627d502bb3786c
Marcos <mcsar1997@gmail.com>
2019-09-13 14:02:15 -0300

    Implementa comunicação com banco de dados

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
+
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+
+
+routes.get('/dev', (req, res) => {
+  return res.json({ message: 'foi' })
+});
 
 module.exports = routes;
